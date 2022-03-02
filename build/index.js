e360e3157605cc1b14d735631b2f5865d5520822
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-02 13:11:40 -0600

    add a method for sorting a linked list

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 //Sort an array of numbers
 const sortNumbers = new NumbersCollection_1.NumbersCollection([5, -2, 78, 0, 13]);
 const sortingNumbers = new Sorter_1.Sorter(sortNumbers);
@@ -13,3 +14,12 @@ const sortCharacters = new CharactersCollection_1.CharactersCollection('kTYopzsQ
 const sortingCharacters = new Sorter_1.Sorter(sortCharacters);
 sortingCharacters.sort();
 console.log(sortCharacters.data);
+//Sort a linked list
+const sortList = new LinkedList_1.LinkedList();
+sortList.add(234);
+sortList.add(-34);
+sortList.add(66);
+sortList.add(-125);
+const sortingList = new Sorter_1.Sorter(sortList);
+sortingList.sort();
+sortList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection,} from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 //Sort an array of numbers
 const sortNumbers = new NumbersCollection([5, -2, 78, 0, 13])
@@ -13,3 +14,14 @@ const sortCharacters = new CharactersCollection('kTYopzsQw');
 const sortingCharacters = new Sorter(sortCharacters);
 sortingCharacters.sort();
 console.log(sortCharacters.data);
+
+//Sort a linked list
+const sortList = new LinkedList();
+sortList.add(234);
+sortList.add(-34);
+sortList.add(66);
+sortList.add(-125);
+
+const sortingList = new Sorter(sortList);
+sortingList.sort();
+sortList.print();
