507444346e935158c2544197ddd7325e5dc4070a
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-02 09:01:12 -0600

    refactor number sorter into its own method

--- build/index.js
+++ build/index.js
@@ -1,23 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-            }
-        }
-    }
-}
-const sortNumbers = new Sorter([5, -2, 78, 0, 13]);
-sortNumbers.sort();
-console.log(sortNumbers);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const sortNumbers = new NumbersCollection_1.NumbersCollection([5, -2, 78, 0, 13]);
+const sorter = new Sorter_1.Sorter(sortNumbers);
+sorter.sort();
+console.log(sortNumbers.data);
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-class Sorter {
+import { Sorter } from "./Sorter";
+import { NumbersCollection,} from "./NumbersCollection";
 
-    constructor(public collection: number[] | string) {
-    }
-
-    sort(): void {
-        
-        const { length } = this.collection;
-
-        for(let i = 0; i < length; i++) {
-            for(let j = 0; j < length - i - 1; j++) {
-
-                if(this.collection instanceof Array) {
-                    if(this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                
-            }
-        }
-    }
-}
-
-const sortNumbers = new Sorter ([5, -2, 78, 0, 13]);
-sortNumbers.sort();
-console.log(sortNumbers);
-
+const sortNumbers = new NumbersCollection([5, -2, 78, 0, 13])
+const sorter = new Sorter(sortNumbers);
+sorter.sort();
+console.log(sortNumbers.data)
