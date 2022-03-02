64f1f9890f3e80c4ecccf8b5813a034b957d0a4f
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-02 10:55:32 -0600

    add sorting script for strings

--- build/index.js
+++ build/index.js
@@ -2,7 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+//Sort an array of numbers
 const sortNumbers = new NumbersCollection_1.NumbersCollection([5, -2, 78, 0, 13]);
-const sorter = new Sorter_1.Sorter(sortNumbers);
-sorter.sort();
+const sortingNumbers = new Sorter_1.Sorter(sortNumbers);
+sortingNumbers.sort();
 console.log(sortNumbers.data);
+//Sort a string of characters
+const sortCharacters = new CharactersCollection_1.CharactersCollection('kTYopzsQw');
+const sortingCharacters = new Sorter_1.Sorter(sortCharacters);
+sortingCharacters.sort();
+console.log(sortCharacters.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection,} from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
+//Sort an array of numbers
 const sortNumbers = new NumbersCollection([5, -2, 78, 0, 13])
-const sorter = new Sorter(sortNumbers);
-sorter.sort();
+const sortingNumbers = new Sorter(sortNumbers);
+sortingNumbers.sort();
 console.log(sortNumbers.data)
+
+//Sort a string of characters
+const sortCharacters = new CharactersCollection('kTYopzsQw');
+const sortingCharacters = new Sorter(sortCharacters);
+sortingCharacters.sort();
+console.log(sortCharacters.data);
